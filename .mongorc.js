4dfdf0a3511b3e676c1c93b5a0033e7587776ae4
Jon Blackburn <me@jonathanblackburn.net>
2013-08-02 08:20:31 -0500

    John Barry's changes to .mongorc.js. Make .hgrc include merge config.

--- .mongorc.js
+++ .mongorc.js
@@ -1,10 +1,18 @@
 var host = db.serverStatus().host;
 var prompt = function() { return db+"@"+host+"> "; }
 
-DBCollection.prototype.pfind = function(){
-            return this.find().pretty();
-};
+DBQuery.prototype._prettyShell = true
 
 conn = new Mongo()
-    db = conn.getDB("localdev")
+db = conn.getDB("localdev")
 
+function sc() {
+     return db.getCollectionNames();
+ }
+
+function dbs() {
+    return db.adminCommand('listDatabases');
+}
+function Qry() {
+    return this.find({});
+}
